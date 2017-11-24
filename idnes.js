9524f5bb7f65c5fe864c1d30bdad5a8b87012da1
fanick1 <fanickbux@gmail.com>
2017-11-24 10:48:48 +0100

    Update idnes.js

--- idnes.js
+++ idnes.js
@@ -11,6 +11,8 @@
 // @run-at document-idle
 // ==/UserScript==
 
+
+window.idneskillervar1 = false;
 function run(){
 
     'use strict';
@@ -18,21 +20,30 @@ function run(){
     console.log('we are doing this');
 
     var tables = document.body.getElementsByTagName('table');
-    erase(tables);
+   erase(tables);
     erase(document.body.getElementsByTagName('iframe'));
-    erase(document.body.getElementsByTagName('meta'));
+       erase(document.body.getElementsByTagName('meta'));
     document.body.onclick = undefined;
-
+    // Your code here...
 }
 
 (function() {
+
     'use strict';
     run();
     setTimeout(run, 1000);
+    if(!window.idneskillervar1){
+        window.idneskillervar1 = true;
+        document.addEventListener('click', killer, true);
+    }
 })();
-
+function killer(e){
+    if(e.target.tagName == 'DIV'){e.stopPropagation();}
+}
 function erase(elements){
     for(let t of elements){
         t.parentElement.removeChild(t);
+
     }
+
 }
